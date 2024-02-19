54a0a6ce02a17fba17a1b7989b182aa986684c09
xjfjdjfj <132452123+xjfjdjfj@users.noreply.github.com>
2024-02-20 01:41:07 +0330

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,18 +3,18 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    APP_ID = 11822563
+    API_HASH = "12bfd8057541544a482312319d7fe4ae"
     # the name to display in your alive message
-    ALIVE_NAME = "Your value"
+    ALIVE_NAME = "xdgdyvxghs"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "postgres://ujhlqgbu:juQLQt4cRuzch0kdZiisiNYFPe5wWEau@jelani.db.elephantsql.com/ujhlqgbu"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "1BJWap1wBuwZ2A7HFyPboF9OfDZSIdDusEAGI8Xr1tj46QFyBo6SaLsPnf8MtI_Ms7b9obnEMGOCm9KQz_tCe5pLSIHnszMFLUFWkwPdXiyF-OoVaQqt4BpOhNNy5gw7MMs1Ot2bjEhV8C8570y5lU6zA_r1oD8zF5Si5kXK-ejgPN9cbKEhcT6Q_XYGNFtKOSBJrUZSnm6_TC2vtAaNVxvDEGt2DMP-U7jaU7KNB2zLtHLQcHFP2KDKjLR1Y5Mj4MTq9-R-eMUI-gyBTJs1vFuBL3VYXzvUrcN_hGwosY3cLAnujVg1_g9KjvFroi5oRI8hi_EJ-tVxwo7iMJ1MhErlCW1m-OFw="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
+    TG_BOT_TOKEN = "7157850786:AAEMradRDG-1Cm0RaYKNnAqtkE6qFoL6RG0"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    PRIVATE_GROUP_BOT_API_ID = -1001979621867
     # command handler
     COMMAND_HAND_LER = "."
     # command hanler for sudo
@@ -22,4 +22,6 @@ class Development(Config):
     # External plugins repo
     EXTERNAL_REPO = "https://github.com/TgCatUB/CatPlugins"
     # if you need badcat plugins set "True"
-    BADCAT = "False"
+    BADCAT = "True"
+    VCMODE = "True"
+    VC_SESSION = "1BJWap1wBuwZ2A7HFyPboF9OfDZSIdDusEAGI8Xr1tj46QFyBo6SaLsPnf8MtI_Ms7b9obnEMGOCm9KQz_tCe5pLSIHnszMFLUFWkwPdXiyF-OoVaQqt4BpOhNNy5gw7MMs1Ot2bjEhV8C8570y5lU6zA_r1oD8zF5Si5kXK-ejgPN9cbKEhcT6Q_XYGNFtKOSBJrUZSnm6_TC2vtAaNVxvDEGt2DMP-U7jaU7KNB2zLtHLQcHFP2KDKjLR1Y5Mj4MTq9-R-eMUI-gyBTJs1vFuBL3VYXzvUrcN_hGwosY3cLAnujVg1_g9KjvFroi5oRI8hi_EJ-tVxwo7iMJ1MhErlCW1m-OFw="
